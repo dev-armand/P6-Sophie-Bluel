f9e19eed9fb373163be7d5d09566bdebb732a10e
Armand Bredean <armand.bredean@gmail.com>
2023-07-20 18:39:07 +0200

    first commit

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,42 +1,44 @@
+const loginButton = document.querySelector('#loginButton');
+loginButton.addEventListener('click', loginForm);
 
-// fonction de connexion
-async function login(email, password) {
+async function handleSuccessfulLogin(token) {
+  // Save the token to sessionStorage
+  sessionStorage.setItem("userId", token);
+
+  // Redirect the user to index.html
+  window.location.href = "index.html";
+}
+
+// Login function
+async function loginForm() {
+  const email = document.querySelector('#email').value;
+  const password = document.querySelector('#password').value;
+
+  const user = {
+    email: email,
+    password: password
+  };
+
   try {
-    // requête GET à l'API pour récupérer les utilisateurs
-    const response = await fetch("http://localhost:5678/api/users/login");
-    const users = await response.json();
-  
-  // recherche utilisateurs
-  const user = users.find(user => user.email === email);
-  //condition si l'utilisateur n'existe pas
-  if (!user) {
-    return "L'utilisateur n'existe pas";
-  }
-  // condition si mot de passe correcte
-  if (user.password === password) {
-    return "Connexion réussie!";
-  } else {
-    return "Mot de passe incorrecte!"
-  }
-  } catch (error) {
-    console.error("Erreur lors de la récupération des utilisateurs :", error);
-    return "Une erreur s'est produite lors de la connexion. ";
-  }
-};
+    const response = await fetch("http://localhost:5678/api/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
 
-// fonction de gestion du formulaire de connexion
-function handleLogin(event){
-  event.preventDefault();
-  const email = document.querySelector("#email").value;
-  const password = document.querySelector("#password").value;
-  const result = login(email, password);
-  result.then(message => {
-    alert(message);
-  });
-};
+    if (!response.ok) {
+      throw new Error("Login failed!");
+    }
 
-// event listener 
-addEventListener("DOMContentLoaded", () => {
-  const loginForm = document.querySelector(".loginForm");
-  loginForm.addEventListener("submit", handleLogin);
-});
+    const data = await response.json();
+    console.log(data);
+    // Call the handleSuccessfulLogin function and pass the token received from the server
+    handleSuccessfulLogin(data.token);
+    
+  } catch (err) {
+    console.log(err);
+    alert("Email ou mot de passe incorrecte !");
+  }
+}
